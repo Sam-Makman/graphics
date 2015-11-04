1bec5503667a4749c7a698fc9e1ab969dbbde917
sam <makman@lclark.edu>
2015-11-04 11:03:22 -0800

    updates

--- 3DFillpoly/D3d_matrixS.c
+++ 3DFillpoly/D3d_matrixS.c
@@ -123,6 +123,7 @@ int D3d_scale (double a[4][4], double b[4][4], double sx, double sy, double sz){
   D3d_mat_mult(b, b, scale);
 return 1;
 }//Done
+
 // a = scale*a  
 // b = b*scale_inverse 
 
@@ -130,36 +131,38 @@ int D3d_rotate_x (double a[4][4], double b[4][4], double radians){
   double rotate[4][4], r2[4][4] ;
   D3d_make_identity(rotate);
   D3d_make_identity(r2);
-  rotate[0][0] = cos(radians);
-  rotate[0][1] = -sin(radians);
-  rotate[1][0] = sin(radians);
   rotate[1][1] = cos(radians);
+  rotate[1][2] = -sin(radians);
+  rotate[2][1] = sin(radians);
+  rotate[2][2] = cos(radians);
   D3d_mat_mult(a, rotate, a);
-  r2[0][0] = cos(-radians);
-  r2[0][1] = -sin(-radians);
-  r2[1][0] = sin(-radians);
   r2[1][1] = cos(-radians);
+  r2[1][2] = -sin(-radians);
+  r2[2][1] = sin(-radians);
+  r2[2][2] = cos(-radians);
     D3d_mat_mult(b,b,r2 );
   return 1;
-}
+}//finished
 // a = rotate*a  
-// b = b*rotate_inverse  
+// b = b*rotate_inverse 
+
+
 int D3d_rotate_y (double a[4][4], double b[4][4], double radians){
   double rotate[4][4], r2[4][4] ;
   D3d_make_identity(rotate);
   D3d_make_identity(r2);
   rotate[0][0] = cos(radians);
-  rotate[0][1] = -sin(radians);
-  rotate[1][0] = sin(radians);
-  rotate[1][1] = cos(radians);
+  rotate[0][2] = sin(radians);
+  rotate[2][0] = -sin(radians);
+  rotate[3][3] = cos(radians);
   D3d_mat_mult(a, rotate, a);
   r2[0][0] = cos(-radians);
-  r2[0][1] = -sin(-radians);
-  r2[1][0] = sin(-radians);
-  r2[1][1] = cos(-radians);
-    D3d_mat_mult(b,b,r2 );
+  r2[0][2] = -sin(-radians);
+  r2[2][0] = sin(-radians);
+  r2[3][3] = cos(-radians);
+  D3d_mat_mult(b,b,r2 );
   return 1;
-}
+}//finished
 
 int D3d_rotate_z (double a[4][4], double b[4][4], double radians){
   double rotate[4][4], r2[4][4] ;
@@ -176,7 +179,29 @@ int D3d_rotate_z (double a[4][4], double b[4][4], double radians){
   r2[1][1] = cos(-radians);
     D3d_mat_mult(b,b,r2 );
   return 1;
+}//finished
+
+
+
+int D3d_cs_rotate_x (double a[4][4], double b[4][4], double cs, double sn){
+  return 0;
 }
+// a = rotate*a  
+// b = b*rotate_inverse  
+
+
+int D3d_cs_rotate_y (double a[4][4], double b[4][4], double cs, double sn){
+  return 0;
+}
+// a = rotate*a  
+// b = b*rotate_inverse  
+
+
+int D3d_cs_rotate_z (double a[4][4], double b[4][4], double cs, double sn){
+  return 0;
+}
+// a = rotate*a  
+// b = b*rotate_inverse  
 
 int D3d_negate_x (double a[4][4], double b[4][4]){
  double neg[4][4] ;
@@ -185,7 +210,7 @@ int D3d_negate_x (double a[4][4], double b[4][4]){
   D3d_mat_mult(a,neg,a);
   D3d_mat_mult(b,b, neg);
   return 1;
-}
+}//finished
 // negate the x....reflects in the y-axis
 // a = reflect*a 
 // b = b*reflect_inverse  
@@ -197,35 +222,37 @@ int D3d_negate_y (double a[4][4], double b[4][4]){
   D3d_mat_mult(a,neg,a);
   D3d_mat_mult(b,b, neg);
 return 1;
-}
+}//finished
 // negate the y....reflects in the x-axis
 // a = reflect*a 
 // b = b*reflect_inverse  
 
 
-int D3d_negate_y (double a[4][4], double b[4][4]){
+int D3d_negate_z (double a[4][4], double b[4][4]){
    double neg[4][4] ;
   D3d_make_identity(neg);
   neg[2][2]=-1;
   D3d_mat_mult(a,neg,a);
   D3d_mat_mult(b,b, neg);
 return 1;
-}
+}//finished
 
 
-int D3d_mat_mult_points (double *X, double *Y, double m[4][4], double *x, double *y, int numpoints){
-double xres[numpoints], yres[numpoints];
+int D3d_mat_mult_points (double *X, double *Y,double  *Z, double m[4][4], double *x, double *y,double *z, int numpoints){
+double xres[numpoints], yres[numpoints], zres[numpoints];
 int i;
 // printf("Mat mult points\n");
 for(i=0; i<numpoints; i++){
-  xres[i] = m[0][0] * x[i] + m[0][1] * y[i] + m[0][2];
-  yres[i] = m[1][0] * x[i] + m[1][1] * y[i] + m[1][2];
+  xres[i] = m[0][0] * x[i] + m[0][1] * y[i] + m[0][2] * z[i] + m[0][3] ;
+  yres[i] = m[1][0] * x[i] + m[1][1] * y[i] + m[1][2] * z[i] + m[1][3];
+  zres[i] = m[2][0] * x[i] + m[2][1] * y[i] + m[2][2] * z[i] + m[2][3];
   // printf("X = %lf, Y = %lf \n", xres[i], yres[i]);
 
 }
 for(i=0; i<numpoints; i++){
   X[i] = xres[i];
   Y[i] = yres[i];
+  Z[i] = zres[i];
 }
 return 1;
 }
@@ -233,6 +260,87 @@ return 1;
 // |Y0 Y1 Y2 ...| = m * |y0 y1 y2 ...|
 // | 1  1  1 ...|       | 1  1  1 ...|
 
-int main(){
 
+
+int D3d_mat_mult_pt (double P[3],
+                     double m[4][4],
+                     double Q[3]) {
+  return 0;
 }
+// multiplies a SINGLE point by a matrix
+// | P[0] |       | Q[0] |
+// | P[1] | = m * | Q[1] |
+// | P[2] |       | Q[2] |
+// |  1   |       |  1   |
+
+// SAFE, user may make a call like 
+// D3d_mat_mult_pt (q,  m,q) ;
+
+
+
+int D3d_x_product (double res[3], double a[3], double b[3]){
+  return 0;
+}
+// res = a x b  , cross product of two vectors
+// SAFE: it is ok to make a call such as
+// D3d_x_product (a,  a,b) or
+// D3d_x_product (b,  a,b) or
+// D3d_x_product (a,  a,a) 
+
+
+
+
+int D3d_make_movement_sequence_matrix (
+                              double mat[4][4],
+                              double inv[4][4],
+                              int num_movements,
+                              int *movement_type_list,
+                              double *parameter_list ){
+  return 0;
+}
+
+     // create a matrix (mat) and its inverse (inv)
+     // that specify a sequence of movements....
+
+     // movement_type_list[k] is an integer that
+     // specifies the type of matrix to be used in the
+     // the k-th movement.  the parameter that each
+     // matrix needs is supplied in parameter_list[k].
+
+     // return 1 if successful, 0 if error
+
+
+     // the codes for movement_type_list are :
+     // 0 - scale x
+     // 1 - scale y
+     // 2 - scale z
+
+     // 3 - rotate x
+     // 4 - rotate y
+     // 5 - rotate z
+   
+     // 6 - translate x
+     // 7 - translate y
+     // 8 - translate z
+
+     // 9  - negate x...reflect in the yz plane
+     // 10 - negate y...relfect in the xz plane
+     // 11 - negate z...reflect in the zy plane
+
+
+
+
+
+int D3d_view (double view[4][4],  double view_inverse[4][4],
+              double eye_x, double eye_y, double eye_z ,
+              double coi_x, double coi_y, double coi_z ,
+              double up_x,  double up_y,  double up_z ){
+  return 0;
+}
+// Construct the view matrix and its inverse given the location
+// of the eye, the center of interest, and an up point.
+// return 1 if successful, 0 otherwise.
+
+// int main(){
+
+// }
--- 3DFillpoly/multishape.c
+++ 3DFillpoly/multishape.c
@@ -1,10 +1,11 @@
 #include <FPT.h>
-#include <D2d_matrix.h>
+#include <D3d_matrixS.c>
 
 #define WINDOW_WIDTH 600
 #define ROTATE_SPEED .05 
-double x[20][1000], y[20][1000], colors[20][1000][3];
-int numpoints[20], numpolys[20], poly_sizes[20][1000], polygons[20][1000][40];
+double x[20][15000], y[20][15000], z[20][15000], colors[20][15000][3];
+int numpoints[20], numpolys[20], poly_sizes[20][15000], polygons[20][15000][100];
+
 void setUp(){
   G_init_graphics(WINDOW_WIDTH,WINDOW_WIDTH + 30);
   G_rgb(0,0,0);
@@ -15,6 +16,7 @@ void setUp(){
 
 
 void loadFiles(int argc, char ** argv){
+	printf("load files");
 int i,j,k;
 FILE * f;
 for(i=1; i<argc; i++){
@@ -22,10 +24,10 @@ for(i=1; i<argc; i++){
 		if(f == NULL){
 			printf("Cannot open file %s \n", argv[i]);
 		}
-	// printf("loading file");
+	printf("loading file\n");
 	fscanf(f , "%d", &numpoints[i -1]);
 	for(j=0; j< numpoints[i-1]; j++){
-		fscanf(f, "%lf %lf ", &x[i - 1][j], &y[i - 1][j]);
+		fscanf(f, "%lf %lf %lf", &x[i - 1][j], &y[i - 1][j], &z[i-1][j]);
 		// printf( "points %lf %lf  \n", x[i - 1][j], y[i - 1][j]);
 	}
 	fscanf( f, "%d", &numpolys[i -1]);
@@ -44,50 +46,67 @@ for(i=1; i<argc; i++){
 }
 
 
-void scaleNfit(int argc){
-int i,j;
-double scale[3][3] ,iscale[3][3];
-for(i=0; i<argc-1;i++){
-	double xmax, xmin, ymax, ymin, xrad, yrad, xcenter, ycenter;
-			xmin = x[i][0];
-			xmax = x[i][0];
-			ymin = y[i][0];
-			ymax = y[i][0];
-	for(j = 0; j<numpoints[i];j++){
-		if(x[i][j] < xmin){
-			xmin = x[i][j];
-		}
-		if(x[i][j] > xmax){
-			xmax = x[i][j];
-		}
-		if(y[i][j] < ymin){
-			ymin = y[i][j];
-		}
-		if(y[i][j] > ymax){
-			ymax = y[i][j];
-		}
-	}
-	xrad = (xmax - xmin)/2;
-	 // printf("xmax = %lf xmin = %lf \n",xmax, xmin );
-	yrad = (ymax - ymin)/2;
-	 // printf("ymax = %lf ymin = %lf \n",ymax, ymin );
-	xcenter = xmax - xrad;
-	ycenter = ymax - yrad;
-	// printf("Xcenter = %lf Ycenter = %lf \n", xcenter, ycenter);
-	double s;
-	if(xrad > yrad){
-		s = xrad;
-	}else{
-		s=yrad;
-	}
-	s = (WINDOW_WIDTH/2)/s;
-	D2d_make_identity(scale);
-	D2d_make_identity(iscale);
-	D2d_translate(scale, iscale, -xcenter, -ycenter);
-	D2d_scale(scale, iscale,s,s);
-	D2d_translate(scale, iscale, WINDOW_WIDTH/2, WINDOW_WIDTH/2);
-	D2d_mat_mult_points(x[i],y[i],scale,x[i],y[i], numpoints[i]);
-}
+// void scaleNfit(int argc){
+// int i,j;
+// double scale[3][3] ,iscale[3][3];
+// for(i=0; i<argc-1;i++){
+// 	double xmax, xmin, ymax, ymin, xrad, yrad, xcenter, ycenter;
+// 			xmin = x[i][0];
+// 			xmax = x[i][0];
+// 			ymin = y[i][0];
+// 			ymax = y[i][0];
+// 	for(j = 0; j<numpoints[i];j++){
+// 		if(x[i][j] < xmin){
+// 			xmin = x[i][j];
+// 		}
+// 		if(x[i][j] > xmax){
+// 			xmax = x[i][j];
+// 		}
+// 		if(y[i][j] < ymin){
+// 			ymin = y[i][j];
+// 		}
+// 		if(y[i][j] > ymax){
+// 			ymax = y[i][j];
+// 		}
+// 	}
+// 	xrad = (xmax - xmin)/2;
+// 	 // printf("xmax = %lf xmin = %lf \n",xmax, xmin );
+// 	yrad = (ymax - ymin)/2;
+// 	 // printf("ymax = %lf ymin = %lf \n",ymax, ymin );
+// 	xcenter = xmax - xrad;
+// 	ycenter = ymax - yrad;
+// 	// printf("Xcenter = %lf Ycenter = %lf \n", xcenter, ycenter);
+// 	double s;
+// 	if(xrad > yrad){
+// 		s = xrad;
+// 	}else{
+// 		s=yrad;
+// 	}
+// 	s = (WINDOW_WIDTH/2)/s;
+// 	D2d_make_identity(scale);
+// 	D2d_make_identity(iscale);
+// 	D2d_translate(scale, iscale, -xcenter, -ycenter);
+// 	D2d_scale(scale, iscale,s,s);
+// 	D2d_translate(scale, iscale, WINDOW_WIDTH/2, WINDOW_WIDTH/2);
+// 	D2d_mat_mult_points(x[i],y[i],scale,x[i],y[i], numpoints[i]);
+// }
+// }
+
+void rotate(int pnum, int axis){
+	double a[4][4], b[4][4];
+ 	D3d_make_identity(a);
+ 	D3d_make_identity(b);
+ 	// D3d_translate(a, b, -WINDOW_WIDTH/2, -WINDOW_WIDTH/2, 0);
+ 	if(axis == 0){
+		D3d_rotate_x(a,b, ROTATE_SPEED);
+ 	}else if(axis == 1){
+		D3d_rotate_y(a,b, ROTATE_SPEED);
+ 	}else if(axis == 2){
+ 		D3d_rotate_z(a,b, ROTATE_SPEED);
+ 	}
+ 	
+ 	// D3d_translate(a, b, WINDOW_WIDTH/2, WINDOW_WIDTH/2,0);
+ 	D3d_mat_mult_points(x[pnum], y[pnum],z[pnum], a, x[pnum], y[pnum],z[pnum], numpoints[pnum]);
 }
 
 void display(int pnum){
@@ -97,24 +116,35 @@ void display(int pnum){
   G_clear(1,1,1);
 // printf("enter disp\n");
 for(i=0;i<numpolys[pnum];i++){
-	double xtemp[poly_sizes[pnum][i]], ytemp[poly_sizes[pnum][i]];
+	double xtemp[poly_sizes[pnum][i]], ytemp[poly_sizes[pnum][i]],ztemp[poly_sizes[pnum][i]];
 	// printf("New display\n");
 	for(j=0; j<poly_sizes[pnum][i];j++){
  		xtemp[j] = x[pnum][polygons[pnum][i][j]];
  		ytemp[j] = y[pnum][polygons[pnum][i][j]];
- 		// printf("X = %lf Y = %lf \n",xtemp[j],ytemp[j] );
+ 		ztemp[j] = z[pnum][polygons[pnum][i][j]];
+ 		// printf("X = %lf Y = %lf  Z = %lf \n",xtemp[j],ytemp[j], ztemp[j] );
 	}
-	
- 	G_rgb(colors[pnum][i][0],colors[pnum][i][1],colors[pnum][i][2]);
- 	G_fill_polygon(xtemp, ytemp ,poly_sizes[pnum][i]);
+	for(j=0; j<poly_sizes[pnum][i];j++){
+		xtemp[j] = xtemp[j]/ztemp[j];
+		ytemp[j] = ytemp[j]/ztemp[j];
+		// xtemp[j] = xtemp[j] + 300;
+		// ytemp[j] = ytemp[j] + 300;
+		// printf("x, y %lf %lf ",xtemp[j], ytemp[j]);
+		}
+
+
+ 	// G_rgb(colors[pnum][i][0],colors[pnum][i][1],colors[pnum][i][2]);
+ 	G_rgb(1,0,0);
+ 	G_polygon(xtemp, ytemp, poly_sizes[pnum][i]);
+ 	// G_fill_polygon(xtemp, ytemp ,poly_sizes[pnum][i]);
  	}
- 	double a[3][3], b[3][3];
- 	D2d_make_identity(a);
- 	D2d_make_identity(b);
- 	D2d_translate(a, b, -WINDOW_WIDTH/2, -WINDOW_WIDTH/2);
- 	D2d_rotate(a,b, ROTATE_SPEED);
- 	D2d_translate(a, b, WINDOW_WIDTH/2, WINDOW_WIDTH/2);
- 	D2d_mat_mult_points(x[pnum], y[pnum], a, x[pnum], y[pnum],numpoints[pnum]);
+ 	// double a[3][3], b[3][3];
+ 	// D2d_make_identity(a);
+ 	// D2d_make_identity(b);
+ 	// D2d_translate(a, b, -WINDOW_WIDTH/2, -WINDOW_WIDTH/2);
+ 	// D2d_rotate(a,b, ROTATE_SPEED);
+ 	// D2d_translate(a, b, WINDOW_WIDTH/2, WINDOW_WIDTH/2);
+ 	// D2d_mat_mult_points(x[pnum], y[pnum], a, x[pnum], y[pnum],numpoints[pnum]);
 
 }
 
@@ -127,20 +157,36 @@ int main(int argc, char ** argv){
 }
 setUp();
 loadFiles(argc, argv);
-scaleNfit(argc);
-
-
-int num;
+// scaleNfit(argc);
+display(0);
+G_wait_key();
+int num, file, axis;
+file = 0;
+axis = 0;
 while(1 == 1){
 	num = G_wait_key();
-	if( num >= 48 && num <= 57){
-		num = num - 48;
-		if(num < argc){
-			display(num);
-		}
+	if(num == 114){
+		rotate(file, axis);
+			display(file);
 	}
-	if(num == 81 || num == 113){
+	else if(num == 81 || num == 113){
 		break;
 	}
+	else if(num ==120){
+		axis = 0;
+	}
+	else if(num == 121){
+		axis = 1;
+	}
+	else if(num = 122){
+		axis = 2;
+	}
+	else if( num >= 48 && num <= 57){
+		file = num - 48;
+		if(file < argc){
+			display(file);
+		}
+	}
+	
 }
 }
