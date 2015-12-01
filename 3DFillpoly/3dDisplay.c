359c801a870a60fc87662e976c1d3cc2433aa2a1
sam <makman@lclark.edu>
2015-11-30 20:35:43 -0800

    finished light model with primative coloring

--- 3DFillpoly/3dDisplay.c
+++ 3DFillpoly/3dDisplay.c
@@ -12,10 +12,11 @@ struct {
 }
 POLYGON;
 
-double x[20][15000], y[20][10000], z[20][10000], colors[20][10000][3];
+double x[20][10000], y[20][10000], z[20][10000], colors[20][10000][3];
 int numpoints[20], numpolys[20], poly_sizes[20][10000], polygons[20][10000][100];
 POLYGON polyLoc[200000];
 int plsize;
+double red, green, blue, intensity;
 
 
 
@@ -40,7 +41,7 @@ void setUp(){
 }
 
 double dot_product( double a[3], double b[3]){
- return((a[0]*b[0]) + (a[1]*b[1]) + (a[2]*b[2]) );
+ return((a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]) );
 }
 
 
@@ -74,15 +75,7 @@ for(i=1; i<argc; i++){
       		fscanf(f,"%lf %lf %lf", &colors[i -1][k][0], &colors[i -1][k][1],  &colors[i -1][k][2]);
     	}
 	}
-	for(i=0; i<m; i++){
-		// printf("%lf \n",polyLoc[i].dist);
-	}
-	// printf("SORT \n");
 	qsort(polyLoc, m,sizeof(POLYGON),compare);
-	for(i=0; i<m; i++){
-		// printf("%lf \n",polyLoc[i].dist);
-	}
-	// printf("plsize = %d \n", m);
 	plsize = m;
 	fclose(f);
 }
@@ -124,9 +117,9 @@ for(i=0; i<argc-1;i++){
 	yrad = (ymax - ymin)/2;
 	zrad = (zmax - zmin)/2;
 	 // printf("ymax = %lf ymin = %lf \n",ymax, ymin );
-	xcenter = xmax - xrad;
-	ycenter = ymax - yrad;
-	zcenter = zmax - zrad;
+	x[i][numpoints[i]+1] = xmax - xrad;
+	y[i][numpoints[i]+1] = ymax - yrad;
+	z[i][numpoints[i]+1] = zmax - zrad;
 	// printf("Xcenter = %lf Ycenter = %lf \n", xcenter, ycenter);
 	double s;
 	
@@ -142,7 +135,6 @@ for(i=0; i<argc-1;i++){
 	D3d_make_identity(scale);
 	D3d_make_identity(iscale);
 	D3d_translate(scale, iscale, -xcenter, -ycenter, -zcenter);
-	// D3d_scale(scale, iscale,1/s,1/s,1/s);r
 	D3d_mat_mult_points(x[i],y[i],z[i],scale,x[i],y[i],z[i], numpoints[i]);
 }
 }
@@ -155,41 +147,7 @@ void rotate(int pnum, int axis, int direction){
  	if(direction == 0){
  		sign = -1;
  	}
- 	double xmax, xmin, ymax, ymin, xrad, yrad, xcenter, ycenter, zmax,zmin,zrad,zcenter;
-			xmin = x[pnum][0];
-			xmax = x[pnum][0];
-			ymin = y[pnum][0];
-			ymax = y[pnum][0];
-			zmin = z[pnum][0];
-			zmax = z[pnum][0];
-			int j;
-	for(j = 0; j<numpoints[pnum];j++){
-		if(x[pnum][j] < xmin){
-			xmin = x[pnum][j];
-		}
-		if(x[pnum][j] > xmax){
-			xmax = x[pnum][j];
-		}
-		if(y[pnum][j] < ymin){
-			ymin = y[pnum][j];
-		}
-		if(y[pnum][j] > ymax){
-			ymax = y[pnum][j];
-		}
-		if(z[pnum][j] < zmin){
-			zmin = z[pnum][j];
-		}
-		if(z[pnum][j] > zmax){
-			zmax = z[pnum][j];
-		}
-	}
-	xrad = (xmax - xmin)/2;
-	yrad = (ymax - ymin)/2;
-	zrad = (zmax - zmin)/2;
-	xcenter = xmax - xrad;
-	ycenter = ymax - yrad;
-	zcenter = zmax - zrad;
-	D3d_translate(a,b,-xcenter,-ycenter,-zcenter);
+	D3d_translate(a,b,-x[pnum][numpoints[pnum]+1],-y[pnum][numpoints[pnum]+1],-z[pnum][numpoints[pnum]+1]);
  	if(axis == 0){
 		D3d_rotate_x(a,b, sign* ROTATE_SPEED);
  	}else if(axis == 1){
@@ -197,12 +155,11 @@ void rotate(int pnum, int axis, int direction){
  	}else if(axis == 2){
  		D3d_rotate_z(a,b,sign*ROTATE_SPEED);
  	}
- 	D3d_translate(a,b,xcenter,ycenter,zcenter);
+ 	D3d_translate(a,b,x[pnum][numpoints[pnum]+1],y[pnum][numpoints[pnum]+1],z[pnum][numpoints[pnum]+1]);
  	D3d_mat_mult_points(x[pnum], y[pnum],z[pnum], a, x[pnum], y[pnum],z[pnum], numpoints[pnum]);
 }
 
 void translate(int pnum, int axis, int direction){
-	// printf("begin translate \n");
 	double a[4][4], b[4][4];
  	D3d_make_identity(a);
  	D3d_make_identity(b);
@@ -211,17 +168,22 @@ void translate(int pnum, int axis, int direction){
  		sign = -1;
  	}
  	if(axis == 0){
+ 		x[pnum][numpoints[pnum]+1] = x[pnum][numpoints[pnum]+1] + (sign * 1);
 		D3d_translate(a , b, 1 *sign, 0, 0);
  	}else if(axis == 1){
+
+ 	y[pnum][numpoints[pnum]+1] = y[pnum][numpoints[pnum]+1] + (sign * 1);
 		D3d_translate(a , b, 0,1*sign ,0);
  	}else if(axis == 2){
+ 		z[pnum][numpoints[pnum]+1] = z[pnum][numpoints[pnum]+1] + (sign * 1);
  		D3d_translate(a , b, 0,0,1*sign );
  	}
+ 	
+ 	
  	D3d_mat_mult_points(x[pnum], y[pnum],z[pnum], a, x[pnum], y[pnum],z[pnum], numpoints[pnum]);
- 	// printf("end translate \n");
 }
 
-void display(int pnum, int sign){
+void display(int pnum, int sign, int specpow, double diffusepro, double ambient, int lightx, int lighty, int lightz){
 	int i, j, k, m;
 	G_rgb(0,0,0);
 	G_clear();
@@ -231,9 +193,6 @@ void display(int pnum, int sign){
 		}else{
 			sign=1;
 		}
-		for(pnum = 0; pnum<20; pnum++){
- // printf("enter disp\n");
-
 int n=0;
 // printf("plsize = %d \n", plsize );
 for(n=0; n<plsize; n++){
@@ -247,39 +206,71 @@ double ztemp[poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]];
  		xtemp[j] = x[polyLoc[n].objnum][polygons[polyLoc[n].objnum][polyLoc[n].polynum][j]];
  		ytemp[j] = y[polyLoc[n].objnum][polygons[polyLoc[n].objnum][polyLoc[n].polynum][j]];
  		ztemp[j] = z[polyLoc[n].objnum][polygons[polyLoc[n].objnum][polyLoc[n].polynum][j]];
- 		// printf("X = %lf Y = %lf  Z = %lf \n",xtemp[j],ytemp[j], ztemp[j] );
+ 	
 	}
-	for(j=0; j<poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum];j++){
-		// printf("here j = %d\n",j);
-		xtemp[j] = xtemp[j]/ztemp[j];
-		ytemp[j] = ytemp[j]/ztemp[j];
-		xtemp[j] = (xtemp[j] * (300/tan(M_PI/3))) + 300;
-		ytemp[j] = (ytemp[j] * (300/tan(M_PI/3))) + 300;
-		// printf("X = %lf Y = %lf  Z = %lf \n",xtemp[j],ytemp[j], ztemp[j] );
-		}
-		double perpendicular[3], first[3], second[3], origin[3];
+		double perpendicular[3], first[3], second[3], eye[3], light[3],reflection[3];
 		first[0] = xtemp[0]-xtemp[1];
 		first[1] = ytemp[0]-ytemp[1];
 		first[2] = ztemp[0]-ztemp[1];
 		second[0] = xtemp[0]-xtemp[2];
 		second[1] = ytemp[0]-ytemp[2];
 		second[2] = ztemp[0]-ztemp[2];
-		origin[0] = 0 - xtemp[0];
-		origin[1] = 0 - xtemp[1];
-		origin[2] = 0 - xtemp[2];
+		eye[0] = xtemp[0] - 300;
+		eye[1] = ytemp[0] - 300;
+		eye[2] = ztemp[0];
+		light[0]= xtemp[0] - lightx;
+		light[1]= ytemp[0] - lighty;
+		light[2]= ztemp[0] - lightz;
 
-		// D3d_x_product(perpendicular, first, second);
-		 // printf("cross product  = %lf , %lf , %lf  \n",perpendicular[0] , perpendicular[1], perpendicular[2]);
-		// double d = dot_product(perpendicular, origin );
-		// double o = sqrt((origin[0] * origin[0])+(origin[1] * origin[1])+(origin[2] * origin[2]));
-		// double p = sqrt((perpendicular[0]*perpendicular[0])+(perpendicular[1]*perpendicular[1])+(perpendicular[2]*perpendicular[2]));
-		// printf("d = %lf \n", d);
-		// double color = sign * acos(d/(o*p));
-		double color = xtemp[0]/600;
-		G_rgb(color,color,color);
- 		G_fill_polygon(xtemp, ytemp, poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]);
- 	
+
+for(j=0; j<poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum];j++){
+		xtemp[j] = xtemp[j]/ztemp[j];
+		ytemp[j] = ytemp[j]/ztemp[j];
+		xtemp[j] = (xtemp[j] * (300/tan(M_PI/3))) + 300;
+		ytemp[j] = (ytemp[j] * (300/tan(M_PI/3))) + 300;
+ 		// printf("X = %lf Y = %lf  Z = %lf \n",xtemp[j],ytemp[j], ztemp[j] );
 }
+
+		D3d_x_product(perpendicular, first, second);
+		double length = sqrt(perpendicular[0]*perpendicular[0] + perpendicular[1]*perpendicular[1] + perpendicular[2]*perpendicular[2]);
+		perpendicular[0] = perpendicular[0]/length;
+		perpendicular[1] = perpendicular[1]/length;
+		perpendicular[2] = perpendicular[2]/length;
+		double lightLength =  sqrt(light[0]*light[0] + light[1]*light[1] + light[2]*light[2]);
+		light[0] = light[0]/lightLength;
+		light[1] = light[1]/lightLength;
+		light[2] = light[2]/lightLength;
+		double eyelength =  sqrt(eye[0]*eye[0] + eye[1]*eye[1] + eye[2]*eye[2]);
+		eye[0] = eye[0]/eyelength;
+		eye[1] = eye[1]/eyelength;
+		eye[2] = eye[2]/eyelength;
+		if(dot_product(perpendicular, light) < 0){
+			perpendicular[0] = -1 * perpendicular[0];
+			perpendicular[1] = -1 * perpendicular[1];
+			perpendicular[2] = -1 * perpendicular[2];
+		}
+		double diffuse = diffusepro * dot_product(perpendicular, light);
+
+		reflection[0] = ((2 * dot_product(perpendicular, light)) * perpendicular[0]) - light[0];
+		reflection[1] = ((2 * dot_product(perpendicular, light)) * perpendicular[1]) - light[1];
+		reflection[2] = ((2 * dot_product(perpendicular, light)) * perpendicular[2]) - light[2];
+		double specular = 0;
+		if(dot_product(eye, reflection) < 0){ 
+			specular = 0;
+		}else {
+			specular = (1 - ambient - diffusepro) * pow((dot_product(eye, reflection)),specpow);	
+		}
+		double color = ambient + diffuse + specular;
+		double ad = ambient + diffuse;
+		if(intensity == ad){
+			G_rgb(red, green, blue);
+		}else if(intensity > ad){
+
+		}else if(intensity < ad){
+
+		}
+		G_rgb(color * red,color * green,color*blue);
+ 		G_fill_polygon(xtemp, ytemp, poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]);
 }
 }
 
@@ -288,7 +279,6 @@ int resize(){
 	for(i=0;i<plsize;i++){
 		polyLoc[i].dist = z[polyLoc[i].objnum][polygons[polyLoc[i].objnum][polyLoc[i].polynum][0]];
 	}
-
 	qsort(polyLoc, plsize,sizeof(POLYGON),compare);
 }
 
@@ -299,11 +289,35 @@ int main(int argc, char ** argv){
 	printf("usage: pgm_name file1.xy file2.xy ... \n");
 	exit(1);
 }
+int specpow;
+double diffusepro, ambient;
+ diffusepro = .4;
+ ambient = .2;
+double x = 100;
+double y=200;
+double z=-50;
+ specpow = 70;
+red = 0;
+green = 0;
+blue = 1;
+intensity = .4;
+printf("enter ambeint  > ");
+scanf("%lf", &ambient);
+printf("enter Diffuse  > ");
+scanf("%lf", &diffusepro);
+printf("enter specular  > ");
+scanf("%d", &specpow);
+int x,y,z;
+printf("enter light coordinates \n");
+scanf("%d %d %d", &x, &y, &z);
+printf("enter inital color");
+scanf("%lf %lf %lf", &red, &green, &blue);
+printf("enter intensity");
+scanf("%lf", &intensity);
 setUp();
 loadFiles(argc, argv);
 scaleNfit(argc);
-display(0, 1);
-// G_wait_key();
+display(0, 1, specpow, diffusepro, ambient, x ,y,z);
 int num, file, axis;
 file = 0;
 axis = 0;
@@ -315,7 +329,7 @@ while(1 == 1){
 	if(num == 114){
 		rotate(file, axis, direction);
 		resize();
-		display(file, sign);
+		display(file, sign, specpow, diffusepro, ambient, x , y, z);
 	}
 	else if(num == 81 || num == 113){//quit
 		break;
@@ -331,8 +345,8 @@ while(1 == 1){
 	}
 	else if(num == 116){//translate 
 		translate(file, axis, direction);
-		// resize();
-		display(file,sign);
+		resize();
+		display(file,sign, specpow, diffusepro, ambient, x , y, z);
 	}
 	else if(num == 99){//change translate direction
 		if(direction == 0){
@@ -344,12 +358,12 @@ while(1 == 1){
 	else if( num >= 48 && num <= 57){//choose file
 		file = num - 48;
 		if(file < argc){
-			display(file, sign);
+			display(file, sign, specpow, diffusepro, ambient, x , y, z);
 		}
 	}else if(num == 115){
 		sign = (sign+1)%2;
 
-		display(file, sign);
+		display(file, sign, specpow, diffusepro, ambient, x , y, z);
 	}else{
 		printf("no command");
 	}
