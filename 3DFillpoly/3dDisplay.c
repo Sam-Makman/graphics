4416fe5c78f46e162f3db81f37c8597f15fc94d0
sam <makman@lclark.edu>
2015-12-05 15:40:42 -0800

    3d clipping complete except for yonder

--- 3DFillpoly/3dDisplay.c
+++ 3DFillpoly/3dDisplay.c
@@ -16,7 +16,7 @@ double x[20][10000], y[20][10000], z[20][10000], colors[20][10000][3];
 int numpoints[20], numpolys[20], poly_sizes[20][10000], polygons[20][10000][100];
 POLYGON polyLoc[200000];
 int plsize;
-double red, green, blue;
+double red[10], green[10], blue[10], hither = 1;
 
 
 
@@ -178,7 +178,78 @@ void translate(int pnum, int axis, int direction){
  	D3d_mat_mult_points(x[pnum], y[pnum],z[pnum], a, x[pnum], y[pnum],z[pnum], numpoints[pnum]);
 }
 
+int  Clip_Polygon_Against_Plane(
+                  double a, double b, double c, double d, 
+                  double *polyx, double *polyy, double *polyz, int size,
+                  double *resx, double *resy, double *resz)
 
+// Clip polygon against the plane ax + by + cz + d = 0,
+// where ax + by + cz + d < 0 is considered IN.
+// Incoming poly defined in arrays  polyx, polyy, polyz  with numverts = size.
+// Clipped result values are stored in arrays  resx, resy, resz
+// The numverts of the clipped result is returned as value of the function.
+
+{
+  int num,i,j ;
+  double x1,y1,z1,x2,y2,z2,x21,y21,z21,den,t,xintsct,yintsct, zintsct ;
+  double s1,s2 ;
+
+  num = 0 ;
+  for (i = 0 ; i < size ; i++) {
+     j = (i + 1) % size ;
+
+     // load up segment to be clipped
+     x1 = polyx[i] ; y1 = polyy[i] ; z1 = polyz[i];
+     x2 = polyx[j] ; y2 = polyy[j] ; z2 = polyz[j];
+
+     // clip line segment (x1,y1)-(x2,y2) against line
+     s1 = (a * x1 + b * y1 + c * z1 + d) ;
+     s2 = (a * x2 + b * y2 + c * z2 + d) ;
+
+     if ((s1 >= 0) && (s2 >= 0)) {
+        // out to out, do nothing
+     } else if ((s1 < 0) && (s2 < 0)) {
+        // in to in
+        resx[num] = x2 ; resy[num] = y2 ; resz[num] = z2; num++ ;
+     } else {
+        // one is in, the other out, so find the intersection
+
+        x21 = x2 - x1 ; y21 = y2 - y1 ; z21 = z2 - z1;
+        den = a*x21 + b*y21 + c*z21;
+        if (den == 0) continue ; // do nothing-should never happen
+        t = -(a*x1 + b*y1 + c*z1 + d)/den ;
+        xintsct = x1 + t*x21 ;
+        yintsct = y1 + t*y21 ;
+        zintsct = z1 + t*z21 ; 
+        if (s1 < 0) { 
+          // in to out
+          resx[num] = xintsct ; resy[num] = yintsct ; resz[num] = zintsct; num++ ;
+        } else  {
+          // out to in
+          resx[num] = xintsct ; resy[num] = yintsct ; resz[num] = zintsct; num++ ;
+          resx[num] = x2      ; resy[num] = y2      ; resz[num] = z2; num++ ;
+        }
+
+     }
+
+
+  } // end for i
+
+  return num ;  // return size of the result poly
+}
+
+int Clip_Polygon_Against_Window(double *polyx, double *polyy, double *polyz, int size){
+	double resx[100], resy[100], resz[100], yonder = 100; 
+
+	size = Clip_Polygon_Against_Plane(0,1,.57,0,polyx,polyy,polyz,size, resx,resy,resz);
+	size = Clip_Polygon_Against_Plane(0,-1,.57,0,resx,resy,resz,size,polyx,polyy,polyz);
+	size = Clip_Polygon_Against_Plane(0,0,1,hither,polyx,polyy,polyz,size,resx,resy,resz);
+	size = Clip_Polygon_Against_Plane(0,0,1,-yonder,resx,resy,resz,size,polyx,polyy,polyz);
+	size = Clip_Polygon_Against_Plane(1,0,.57,0,polyx,polyy,polyz,size,resx,resy,resz);
+	size = Clip_Polygon_Against_Plane(-1,0,.57,0,resx,resy,resz,size,polyx,polyy,polyz);
+	return size;
+
+}
 
 void display(int pnum, int sign, int specpow, double diffusepro, double ambient, double lightx, double lighty, double lightz){
 	int i, j, k, m;
@@ -193,9 +264,9 @@ void display(int pnum, int sign, int specpow, double diffusepro, double ambient,
 		}
 int n=0;
 for(n=0; n<plsize; n++){
-double xtemp[poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]];
-double ytemp[poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]];
-double ztemp[poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]];
+double xtemp[100];
+double ytemp[100];
+double ztemp[100];
 	for(j=0; j<poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum];j++){
  		xtemp[j] = x[polyLoc[n].objnum][polygons[polyLoc[n].objnum][polyLoc[n].polynum][j]];
  		ytemp[j] = y[polyLoc[n].objnum][polygons[polyLoc[n].objnum][polyLoc[n].polynum][j]];
@@ -203,36 +274,47 @@ double ztemp[poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]];
 
  	
 	}
+
+		int tempsize = poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum];
 		double perpendicular[3], first[3], second[3], eye[3], light[3],reflection[3];
-		first[0] = xtemp[0]-xtemp[1];
+
+		
+
+
+	 tempsize = Clip_Polygon_Against_Window(xtemp,ytemp,ztemp, poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]);
+	 if(tempsize < 1)continue;
+
+	 	first[0] = xtemp[0]-xtemp[1];
 		first[1] = ytemp[0]-ytemp[1];
 		first[2] = ztemp[0]-ztemp[1];
+
 		second[0] = xtemp[0]-xtemp[2];
 		second[1] = ytemp[0]-ytemp[2];
 		second[2] = ztemp[0]-ztemp[2];
+
 		eye[0] = 0 - xtemp[0] ;
 		eye[1] = 0 - ytemp[0] ;
-		eye[2] = 0 - ztemp[0];
+		eye[2] = 0 - ztemp[0] ;
+
 		light[0]= lightx - xtemp[0] ;
 		light[1]= lighty - ytemp[0] ;
-		light[2]= lightz - ztemp[0];
+		light[2]= lightz - ztemp[0] ;
 
+		double xfinal[tempsize], yfinal[tempsize];
+for(j=0; j<tempsize;j++){
 
-for(j=0; j<poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum];j++){
-
-		xtemp[j] = xtemp[j]/ztemp[j];
-		ytemp[j] = ytemp[j]/ztemp[j];
-		xtemp[j] = (xtemp[j] * (300/tan(halfAngle))) + 300;
-		ytemp[j] = (ytemp[j] * (300/tan(halfAngle))) + 300;
-
-
+		xfinal[j] = xtemp[j]/ztemp[j];
+		yfinal[j] = ytemp[j]/ztemp[j];
+		xfinal[j] = (xfinal[j] * (300/tan(halfAngle))) + 300;
+		yfinal[j] = (yfinal[j] * (300/tan(halfAngle))) + 300;
 }
 
-		D3d_x_product(perpendicular, first, second);
+		D3d_x_product(perpendicular,first,second);
 		double length = sqrt(perpendicular[0]*perpendicular[0] + perpendicular[1]*perpendicular[1] + perpendicular[2]*perpendicular[2]);
 		perpendicular[0] = perpendicular[0]/length;
 		perpendicular[1] = perpendicular[1]/length;
 		perpendicular[2] = perpendicular[2]/length;
+
 		double lightLength =  sqrt(light[0]*light[0] + light[1]*light[1] + light[2]*light[2]);
 		light[0] = light[0]/lightLength;
 		light[1] = light[1]/lightLength;
@@ -274,30 +356,28 @@ for(j=0; j<poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum];j++){
 
 		if(intensity <= ambient + diffusepro){
 			double f = intensity/ (ambient + diffusepro);
-			redt = f*red;
-			greent = f* green;
-			bluet = f*blue;
+			redt = f*red[polyLoc[n].objnum];
+			greent = f* green[polyLoc[n].objnum];
+			bluet = f*blue[polyLoc[n].objnum];
 		}
 		else{
 			double I[3];
-			I[0] = 1-red;
-			I[1] = 1-green;
-			I[2] = 1-blue;
+			I[0] = 1-red[polyLoc[n].objnum];
+			I[1] = 1-green[polyLoc[n].objnum];
+			I[2] = 1-blue[polyLoc[n].objnum];
 			double f = (intensity - ambient - diffusepro)/ (1 - ambient - diffusepro);
-			redt = f*I[0] + red;
-			greent = f*I[1] + green;
-			bluet = f*I[2] + blue;
+			redt = f*I[0] + red[polyLoc[n].objnum];
+			greent = f*I[1] + green[polyLoc[n].objnum];
+			bluet = f*I[2] + blue[polyLoc[n].objnum];
 		}
 	
 	
 
 
-		//	printf("red = %lf   green = %lf  blue = %lf  intensity = %lf\n",redt,greent,bluet, intensity) ;		
 		G_rgb(redt,greent,bluet);
-		// G_rgb(intensity,intensity,intensity) ;
- 		G_fill_polygon(xtemp, ytemp, poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]);
+ 		G_fill_polygon(xfinal, yfinal, tempsize);
  		G_rgb(0,0,0);
- 		G_polygon(xtemp, ytemp, poly_sizes[polyLoc[n].objnum][polyLoc[n].polynum]);
+ 		G_polygon(xfinal, yfinal, tempsize);
 }
 }
 
@@ -318,29 +398,31 @@ int main(int argc, char ** argv){
 }
 int specpow;
 double diffusepro, ambient;
- diffusepro = .5;
- ambient = .2;
+ // diffusepro = .5;
+ // ambient = .2;
 double x = -200;
 double y=-100;
 double z=50;
- specpow = 70;
-red = 1;
-green = 0;
-blue = 0;
+ // specpow = 70;
+// red = 1;
+// green = 0;
+// blue = 0;
 
-// printf("enter ambeint  > ");
-// scanf("%lf", &ambient);
-// printf("enter Diffuse  > ");
-// scanf("%lf", &diffusepro);
-// printf("enter specular  > ");
-// scanf("%d", &specpow);
-// int x,y,z;
-// printf("enter light coordinates \n");
-// scanf("%d %d %d", &x, &y, &z);
-// printf("enter inital color");
-// scanf("%lf %lf %lf", &red, &green, &blue);
-// printf("enter intensity");
-// scanf("%lf", &intensity);
+printf("enter ambeint  > ");
+scanf("%lf", &ambient);
+printf("enter Diffuse  > ");
+scanf("%lf", &diffusepro);
+printf("enter specular  > ");
+scanf("%d", &specpow);
+printf("enter light coordinates \n");
+scanf("%lf %lf %lf", &x, &y, &z);
+
+int i = 0;
+for(i=0;i< argc-1;i++){
+	printf("enter object %d color \n", i);
+scanf("%lf %lf %lf", &red[i], &green[i], &blue[i]);
+}
+
 setUp();
 loadFiles(argc, argv);
 scaleNfit(argc);
@@ -381,6 +463,9 @@ while(1 == 1){
 		}else{
 			direction=0;
 		}
+	}else if(num == 'h'){//adjust hither
+		(sign==0) ? (hither+=.1 ): (hither-=.1);
+		display(file, sign, specpow, diffusepro, ambient, x , y, z);
 	}
 	else if( num >= 48 && num <= 57){//choose file
 		file = num - 48;
